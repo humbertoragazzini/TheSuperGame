308c38e98837795093aa2c84791c432d15fe4e72
beto <beto_vicio@hotmail.com>
2024-11-21 20:37:18 +0000

    added explanation and comments

--- src/Level.js
+++ src/Level.js
@@ -1,11 +1,22 @@
 import { RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 
+// we create a box geometry that we can reuse in all places by just changing the scale of the mesh using it
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
+/**
+ *  This is the first block of the game and is the start of all the levels
+ * @param {object} props - The property passed to the component
+ * @param {[number,number,number]} - here we have the x - y - z position property example:
+ *    - `position[0]`: X-coordinate
+ *    - `position[1]`: Y-coordinate
+ *    - `position[2]`: Z-coordinate
+ * @returns  {JSX.Element} as a return we return the start floor of the game
+ */
 function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
+      {/* floor */}
       <mesh
         geometry={boxGeometry}
         position={[0, -0.1, 0]}
