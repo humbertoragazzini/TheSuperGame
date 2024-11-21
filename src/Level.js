2046a44acc3c4a22148dbd060cb59f0b6ee0e170
beto <beto_vicio@hotmail.com>
2024-11-21 20:25:38 +0000

    added floor but this time from a instance of boxgeometry from threejs

--- src/Level.js
+++ src/Level.js
@@ -1,10 +1,17 @@
 import { RigidBody } from "@react-three/rapier";
+import * as THREE from "three";
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
 function BlockStart({ position = [0, 0, 0] }) {
   return (
     <group position={position}>
-      <mesh position={[0, -0.1, 0]} receiveShadow>
-        <boxGeometry args={[4, 0.2, 4]}></boxGeometry>
+      <mesh
+        geometry={boxGeometry}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      >
         <meshStandardMaterial color={"red"}></meshStandardMaterial>
       </mesh>
     </group>
