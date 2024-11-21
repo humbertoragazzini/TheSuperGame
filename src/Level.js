6c575382995684a6279ecfd51c823a6c1db85d4d
beto <beto_vicio@hotmail.com>
2024-11-21 23:23:27 +0000

    added spinner testing position

--- src/Level.js
+++ src/Level.js
@@ -1,4 +1,6 @@
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
+import { useRef } from "react";
 import * as THREE from "three";
 
 // we create a box geometry that we can reuse in all places by just changing the scale of the mesh using it
@@ -25,10 +27,76 @@ function BlockStart({ position = [0, 0, 0] }) {
       <mesh
         geometry={boxGeometry}
         material={matFloor1}
-        position={[0, -0.1, 0]}
+        position={[0, 0, 0]}
         scale={[4, 0.2, 4]}
         receiveShadow
-      ></mesh>
+      />
+    </group>
+  );
+}
+
+// old
+/**
+ *  This component is a block class obstacule with a spinning mesh that change y x and z position
+ *
+ *  @param {object} props - The property passed to the component
+ *  @param {[number,number,number]} - min position of the translation
+ *  @param {[number,number,number]} - max position of the translation
+ *  @param {number} - total force speed of the spinner
+ *
+ * @returns {JSX.Element} The final return is a block obstacul spinner with the min max and speed set it.
+ */
+
+// new
+/**
+ * This component is a block obstacle with a spinning mesh that moves within defined bounds.
+ * It can change its position (x, y, z) and spins at a configurable speed.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {[number, number, number]} [props.minTranslation=[0, 0, 0]] - The minimum position for the translation (x, y, z).
+ * @param {[number, number, number]} [props.maxTranslation=[0, 0, 0]] - The maximum position for the translation (x, y, z).
+ * @param {number} [props.speed=1] - The rotational speed of the spinner.
+ *
+ * @returns {JSX.Element} A JSX element representing the spinning block obstacle.
+ */
+
+function BlockSpinner({
+  position = [0, 0, 0],
+  minTranslation = [0, 0, 0],
+  maxTranslation = [0, 0, 0],
+  speed = 1,
+}) {
+  const spinner = useRef();
+  useFrame((state, delta) => {
+    const clock = state.clock.getElapsedTime();
+    const eulerRotation = new THREE.Euler(0, clock, 0); // Rotate around Y-axis
+    const quaternion = new THREE.Quaternion();
+    quaternion.setFromEuler(eulerRotation);
+    spinner.current.setNextKinematicTranslation({
+      x: position[0],
+      y: 0 + 0.1,
+      z: position[1],
+    });
+    spinner.current.setNextKinematicRotation(quaternion);
+  });
+  return (
+    <group position={position}>
+      <RigidBody ref={spinner} type="kinematicPosition">
+        <mesh
+          material={matObstacule}
+          geometry={boxGeometry}
+          scale={[0.25, 0.25, 3.5]}
+        ></mesh>
+      </RigidBody>
+      <RigidBody type="fixed">
+        <mesh
+          geometry={boxGeometry}
+          material={matFloor1}
+          position={[0, 0, 0]}
+          scale={[4, 0.2, 4]}
+          receiveShadow
+        />
+      </RigidBody>
     </group>
   );
 }
@@ -37,6 +105,7 @@ export default function Level() {
   return (
     <>
       <BlockStart position={[0, -0.1, 0]}></BlockStart>
+      <BlockSpinner position={[4, -0.1, 0]}></BlockSpinner>
     </>
   );
 }
