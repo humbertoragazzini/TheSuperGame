b610b5fb97b0623bea22b2a3f99c792bfd7385e5
beto <beto_vicio@hotmail.com>
2024-11-24 18:28:09 +0000

    added counter and blocks

--- src/components/Level.jsx
+++ src/components/Level.jsx
@@ -13,10 +13,6 @@ export default function Level() {
   return (
     <>
       <BlockStart position={[0, -0.1, 0]}></BlockStart>
-      <BlockSpinner position={[4, -0.1, 0]}></BlockSpinner>
-      <BlockAxe position={[8, -0.1, 0]}></BlockAxe>
-      <BlockLimboBar position={[12, -0.1, 0]}></BlockLimboBar>
-      <BlockEnd position={[16, -0.1, 0]}></BlockEnd>
     </>
   );
 }
