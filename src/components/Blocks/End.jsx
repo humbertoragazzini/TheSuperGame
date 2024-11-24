63bc027765298339e9aef8e4cd96646af742010a
beto <beto_vicio@hotmail.com>
2024-11-24 18:17:33 +0000

    added donut to the end level like a price for complete it

--- src/components/Blocks/End.jsx
+++ src/components/Blocks/End.jsx
@@ -7,20 +7,34 @@
  *    - `position[2]`: Z-coordinate
  * @returns  {JSX.Element} as a return we return the start floor of the game
  */
+import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import { boxGeometry } from "../../Geometries/Geometries";
 import { matFloor1, matFloor2, matObstacule } from "../../Materials/Materials";
+import { useGLTF } from "@react-three/drei";
 
 export default function BlockEnd({ position = [0, 0, 0] }) {
+  const { nodes } = useGLTF("./donut.glb");
+  console.log(nodes);
+
   return (
     <group position={position}>
       {/* floor */}
-      <mesh
-        geometry={boxGeometry}
-        material={matFloor1}
-        position={[0, 0, 0]}
-        scale={[4, 0.2, 4]}
-        receiveShadow
-      />
+      <RigidBody type="fixed">
+        <mesh
+          geometry={boxGeometry}
+          material={matFloor1}
+          position={[0, 0, 0]}
+          scale={[4, 0.2, 4]}
+          receiveShadow
+        />
+      </RigidBody>
+      <RigidBody type="fixed">
+        <primitive
+          object={nodes.Scene}
+          position={[0, 0.23, 0]}
+          castShadow
+        ></primitive>
+      </RigidBody>
     </group>
   );
 }
