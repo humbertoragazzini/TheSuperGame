c8388fa5c91cd5a0053a0d89cca65675d3f9bb3b
beto <beto_vicio@hotmail.com>
2024-11-24 17:28:47 +0000

    splitting code

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls } from "@react-three/drei";
 import Lights from "./Lights.jsx";
-import Level from "./Level.js";
 import { Physics } from "@react-three/rapier";
+import Level from "./components/Level.jsx";
 
 export default function Experience() {
   return (
---
+++ src/Materials/materials.js
@@ -0,0 +1,4 @@
+export const matFloor1 = new THREE.MeshStandardMaterial({ color: "green" });
+export const matFloor2 = new THREE.MeshStandardMaterial({ color: "blue" });
+export const matObstacule = new THREE.MeshStandardMaterial({ color: "red" });
+export const matWall = new THREE.MeshStandardMaterial({ color: "pink" });
