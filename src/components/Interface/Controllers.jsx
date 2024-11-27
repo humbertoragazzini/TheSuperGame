09efb213564983a60e2628434c9936eb1c4024d9
Humberto Ragazzini <beto_vicio@hotmail.com>
2024-11-27 23:50:11 +0000

    added controllers

--- src/components/Interface/Controllers.jsx
+++ src/components/Interface/Controllers.jsx
@@ -3,48 +3,133 @@ import { useFrame } from "@react-three/fiber";
 import { useEffect, useState } from "react";
 
 export default function Controllers() {
-  const [forward, setForward] = useState();
+  // const [subscribeKeys, getKeys] = useKeyboardControls();
+  // const [, set] = useKeyboardControls();
 
-  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const handlePressForward = () => {};
+  const handleReleaseForward = () => {};
 
-  useEffect(() => {
-    console.log(getKeys);
-  }, []);
+  const ArrowUpPress = () => {
+    const keyEvent = new KeyboardEvent("keydown", {
+      key: "ArrowUp",
+      code: "ArrowUp",
+      keyCode: 38, // Deprecated but still needed for some older browsers
+      bubbles: true,
+    });
 
-  // useFrame((state, delta) => {
-  //   const impulse = { x: 0, y: 0, z: 0 };
-  //   const torque = { x: 0, y: 0, z: 0 };
-  //   const impulseStrenght = 5 * delta;
-  //   const torqueStrenght = 1 * delta;
-  //   if (forward) {
-  //     impulse.x = impulseStrenght;
-  //   }
-  //   if (backward) {
-  //     impulse.x = -impulseStrenght;
-  //   }
-  //   if (leftward) {
-  //     impulse.z = -impulseStrenght;
-  //   }
-  //   if (rightward) {
-  //     impulse.z = impulseStrenght;
-  //   }
-  // });
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowUp");
+  };
+  const ArrowUpRelease = () => {
+    const keyEvent = new KeyboardEvent("keyup", {
+      key: "ArrowUp",
+      code: "ArrowUp",
+      keyCode: 38, // Deprecated but still needed for some older browsers
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowUp");
+  };
+  const ArrowDownPress = () => {
+    const keyEvent = new KeyboardEvent("keydown", {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      keyCode: 38, // Deprecated but still needed for some older browsers
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowUp");
+  };
+  const ArrowDownRelease = () => {
+    const keyEvent = new KeyboardEvent("keyup", {
+      key: "ArrowDown",
+      code: "ArrowDown",
+      keyCode: 38, // Deprecated but still needed for some older browsers
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowUp");
+  };
+
+  const ArrowLeftPress = () => {
+    const keyEvent = new KeyboardEvent("keydown", {
+      key: "ArrowLeft",
+      code: "ArrowLeft",
+      keyCode: 37, // KeyCode for Left Arrow
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowLeft");
+  };
+
+  const ArrowLeftRelease = () => {
+    const keyEvent = new KeyboardEvent("keyup", {
+      key: "ArrowLeft",
+      code: "ArrowLeft",
+      keyCode: 37, // KeyCode for Left Arrow
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key release: ArrowLeft");
+  };
+
+  const ArrowRightPress = () => {
+    const keyEvent = new KeyboardEvent("keydown", {
+      key: "ArrowRight",
+      code: "ArrowRight",
+      keyCode: 39, // KeyCode for Right Arrow
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key press: ArrowRight");
+  };
+
+  const ArrowRightRelease = () => {
+    const keyEvent = new KeyboardEvent("keyup", {
+      key: "ArrowRight",
+      code: "ArrowRight",
+      keyCode: 39, // KeyCode for Right Arrow
+      bubbles: true,
+    });
+
+    document.dispatchEvent(keyEvent);
+    console.log("Simulated key release: ArrowRight");
+  };
+
   return (
     <div className="fixed bottom-0 h-[50vh] w-screen z-[9999]">
       <button
-        onTouchStart={(e) => setForward(true)}
-        onTouchEnd={(e) => setForward(false)}
+        onTouchStart={(e) => ArrowUpPress()}
+        onTouchEnd={(e) => ArrowUpRelease()}
         className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50"
       >
-        up
+        Forward
       </button>
-      <button className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50">
-        down
+      <button
+        onTouchStart={(e) => ArrowDownPress()}
+        onTouchEnd={(e) => ArrowDownRelease()}
+        className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50"
+      >
+        Backward
       </button>
-      <button className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50">
+      <button
+        onTouchStart={(e) => ArrowLeftPress()}
+        onTouchEnd={(e) => ArrowLeftRelease()}
+        className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50"
+      >
         left
       </button>
-      <button className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50">
+      <button
+        onTouchStart={(e) => ArrowRightPress()}
+        onTouchEnd={(e) => ArrowRightRelease()}
+        className="w-1/2 bg-slate-500 h-1/2 border-2 border-white opacity-50"
+      >
         right
       </button>
     </div>
