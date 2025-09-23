123d6b969be2bd77eb6991feeb07ad6cb985cca6
humberto ragazzini <beto_vicio@hotmail.com>
2025-09-23 18:32:16 +0000

    creating a new type of component button

--- src/components/Interface/Interface.jsx
+++ src/components/Interface/Interface.jsx
@@ -2,57 +2,50 @@ import Controllers from "./Controllers";
 
 export default function Interface() {
   return (
-    <div className="w-screen h-screen fixed top-0 left-0">
+    <div className="fixed top-0 left-0 w-screen h-screen">
       {/* Menu */}
-      <div className="absolute right-0 top-0 m-4 rounded-md w-12 h-12 bg-red-600 hover:bg-green-400">
+      <div className="absolute top-0 right-0 w-12 h-12 m-4 bg-red-600 rounded-md hover:bg-green-400">
         Menu
       </div>
       <div className="w-full h-full bg-[rgba(155,25,180,0.5)] backdrop-blur-md flex flex-col justify-center items-center">
-        {/* Start */}
-        <h2 className="text-3xl text-white font-bold">Start</h2>
-        <h2 className="text-3xl text-white font-bold">Settings</h2>
-        <h2 className="text-3xl text-white font-bold">Levels</h2>
-        <h2 className="text-3xl text-white font-bold">About</h2>
-        <h2 className="text-3xl text-white font-bold">Github</h2>
+        <div>
+          <MenuButton>START GAME</MenuButton>
+        </div>
+        <div>
+          <MenuButton>SCORES</MenuButton>
+        </div>
+        <div>
+          <MenuButton>SETTINGS</MenuButton>
+        </div>
+        <div>
+          <MenuButton>ABOUT US</MenuButton>
+        </div>
       </div>
 
       {/* Player stat */}
       <div className="fixed top-0 left-0 bg-[rgba(1,1,1,0.5)] z-50 hidden">
         <div className="w-screen h-[90px] flex justify-between items-center">
-          <button className="text-4xl bg-slate-500 p-4 w-1/3 rounded-full text-white font-bold m-3">
+          <button className="w-1/3 p-4 m-3 text-4xl font-bold text-white rounded-full bg-slate-500">
             Player Name
           </button>
-          <button className="text-6xl bg-slate-500 p-4 w-1/3 rounded-full text-white font-bold m-3">
+          <button className="w-1/3 p-4 m-3 text-6xl font-bold text-white rounded-full bg-slate-500">
             0.00
           </button>
-          <button className="text-4xl bg-slate-500 p-4 w-1/3 rounded-full text-white font-bold m-3">
+          <button className="w-1/3 p-4 m-3 text-4xl font-bold text-white rounded-full bg-slate-500">
             Points: 999
           </button>
         </div>
       </div>
-      <div className="fixed hidden w-screen h-screen flex flex-col justify-center items-center z-50 hidden">
-        <div>
-          <button className="text-4xl text-white font-bold m-3">
-            START GAME
-          </button>
-        </div>
-        <div>
-          <button className="text-4xl text-white font-bold m-3">SCORES</button>
-        </div>
-        <div>
-          <button className="text-4xl text-white font-bold m-3">
-            SETTINGS
-          </button>
-        </div>
-        <div>
-          <button className="text-4xl text-white font-bold m-3">
-            ABOUT US
-          </button>
-        </div>
-      </div>
-
       {/* Controls */}
       {/* <Controllers></Controllers> */}
     </div>
+  );
+}
+
+function MenuButton({ children, onClick }) {
+  return (
+    <button className="text-[70px] font-bold text-white lg:m-5">
+      {children}
+    </button>
   );
 }
